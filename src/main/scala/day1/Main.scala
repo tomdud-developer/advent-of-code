ded02e07fb14a2207ed40ca35ee34a3ff4de958f
tomdud-developer <tomasz.dudzik.it@gmail.com>
2023-12-01 21:28:26 +0100

    AOF-2023 Day1 part2 - example

--- src/main/scala/day1/Main.scala
+++ src/main/scala/day1/Main.scala
@@ -1,13 +1,53 @@
 package day1
 
+import scala.collection.immutable.Map
+
 object Main {
   def main(args: Array[String]): Unit = {
-    val file = scala.io.Source.fromFile("src/main/resources/day1/input.txt")
+    val file = scala.io.Source.fromFile("src/main/resources/day1/sample2.txt")
 
-    def getFirstDigit(str: String): Int = str.toCharArray.find(_.isDigit).map(_.toInt).get //unsafe get
-    def getLastDigit(str: String): Int = str.toCharArray.findLast(_.isDigit).map(_.toInt).get //unsafe get
+    def getTextDigit(str: String): Option[Int] = {
+      Map(
+        1 -> "one",
+        2 -> "two",
+        3 -> "three",
+        4 -> "four",
+        5 -> "five",
+        6 -> "six",
+        7 -> "seven",
+        8 -> "eight",
+        9 -> "nine"
+      ).find(digit => str.contains(digit._2)).map(_._1)
+    }
 
-    val result = file.getLines().map(line => s"${getFirstDigit(line)}${getLastDigit(line)}".toInt).sum
+    def getSlicesFromBegin(str: String) = (1 to str.length).map(i => str.slice(0, i))
+    def getSlicesFromEnd(str: String) = (1 to str.length).map(i => str.slice(str.length - i, str.length))
+
+    def getFirstDigit(str: String): Int =
+      getSlicesFromBegin(str).map(slice => {
+          if (slice.last.isDigit) {
+            Some(slice.last.asDigit)
+          } else {
+            getTextDigit(slice)
+          }
+        })
+        .find(_.isDefined).get.get // not elegant
+
+    def getLastDigit(str: String): Int =
+      getSlicesFromEnd(str).map(slice => {
+          println("slice: " + slice)
+          if (slice.head.isDigit) {
+            Some(slice.head.asDigit)
+          } else {
+            getTextDigit(slice)
+          }
+        })
+        .find(_.isDefined).get.get // not elegant
+
+
+    val result = file.getLines().map(line => {
+      s"${getFirstDigit(line)}${getLastDigit(line)}".toInt
+    }).sum
 
     println(result)
   }
